811fa740b638665c826d3f055bca305cd6a713ac
Phil <me@Phils-MacBook-Air.local>
2019-10-30 19:46:08 -0500

    Initial commit of data fetching to graphql API

--- src/views/Customer/Customer.js
+++ src/views/Customer/Customer.js
@@ -1,83 +1,142 @@
 import React, {Component, useState} from 'react';
-import Select from "react-select";
-
-
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import DDown from '../Components/DDown';
+import BaseSelect from "react-select";
 import PropTypes from "prop-types";
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Table } from 'reactstrap';
+import { loadPartialConfig } from '@babel/core';
+import ReactTable from 'react-table'
 
-class Customer extends Component {
-    render() {
+// const GET_STATES = gql`
+//     {
+//     allStates {
+//       state_id,
+//       state_name
+//   }
+// }
+// `;
+const columns = [
+  {
+    Header: "ID",
+    accessor: "state_id"
+  },
+  {
+    Header: "State Name",
+    accessor: "state_name"
+  }
+]
+
+const Customer = () => (
+  <Query query={
+    gql` {
+    allStates {
+      state_id,
+      state_name
+  }
+
+}
+`
+}>
+   { ({loading, error, data}) => {
+   if(loading) return <p>Good things take time....</p>
+   if (error) return <p>Something went wrong</p>
+
+   return <ReactTable
+    data={data.allStates}
+    columns={columns}
+   
+   />
+   }}
+  </Query>
+);
+
+// const Select = props => (
+//   <DDown
+//     {...props}
+//     SelectComponent={BaseSelect}
+//     options={null}
+//   />
+// );
+
+// class Customer extends Component {
+//     render() {
       
-      return (
-        <form>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="fname">First Name</label>
-              <input type="fname" class="form-control" id="fname" placeholder="First Name"/>
-            </div>
-            <div class="form-group col-md-6">
-              <label for="lname">Last Name</label>
-              <input type="lname" class="form-control" id="lname" placeholder="Last Name"/>
-            </div>
-          </div>
-          <div class="form-row">
-            <div class="form-group col-md-4">
-              <label for="phone">Phone Number</label>
-              <input type="phone" class="form-control" id="phone" placeholder="999-990-0090"/>
-            </div>
-            <div class="form-group col-md-4">
-              <label for="email">Email</label>
-              <input type="email" class="form-control" id="email" placeholder="jane@yahoo.com"/>
-            </div>
-            <div class="form-group col-md-4">
-              <label for="instagram">Instagram</label>
-              <input type="instagram" class="form-control" id="instagram" placeholder="@fit_preps"/>
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="address">Street Address</label>
-            <input type="text" class="form-control" id="adress" placeholder="1234 Main St"/>
-          </div>
+//       return (
+//         <Form>
+//           <div class="form-row">
+//             <div class="form-group col-md-6">
+//               <label for="fname">First Name <i className="text-danger">required</i></label>
+//               <Input name='fname' class="form-control" placeholder="First Name" validations={[]}/>
+//             </div>
+//             <div class="form-group col-md-6">
+//               <label for="lname">Last Name <i className="text-danger">required</i></label>
+//               <input type="lname" class="form-control" id="lname" placeholder="Last Name"/>
+//             </div>
+//           </div>
+//           <div class="form-row">
+//             <div class="form-group col-md-4">
+//               <label for="phone">Phone Number <i className="text-danger">required</i></label>
+//               <input type="phone" class="form-control" id="phone" placeholder="999-990-0090"/>
+//             </div>
+//             <div class="form-group col-md-4">
+//               <label for="email">Email <i className="text-danger">required</i></label>
+//               <input type="email" class="form-control" id="email" placeholder="jane@yahoo.com"/>
+//             </div>
+//             <div class="form-group col-md-4">
+//               <label for="instagram">Instagram</label>
+//               <input type="instagram" class="form-control" id="instagram" placeholder="@fit_preps"/>
+//             </div>
+//           </div>
+//           <div class="form-group">
+//             <label for="address">Street Address</label>
+//             <input type="text" class="form-control" id="adress" placeholder="1234 Main St"/>
+//           </div>
           
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="city">City</label>
-              <input type="text" class="form-control" id="city"/>
-            </div>
-            <div class="form-group col-md-4">
-              <label for="state">State</label>
-              <Select id="inputState" class="form-control"
-                  closeMenuOnSelect={false}
-                  options={null}
-                  hideSelectedOptions={false}
-                  backspaceRemovesValue={false}
-                  placeholder="Select a state"
-                  />
+//           <div class="form-row">
+//             <div class="form-group col-md-6">
+//               <label for="city">City</label>
+//               <input type="text" class="form-control" id="city"/>
+//             </div>
+//             <div class="form-group col-md-4">
+//               <label for="state">State</label>
+//               <Select id="inputState" class="form-control"
+//                   closeMenuOnSelect={false}
+//                   options={null}
+//                   hideSelectedOptions={false}
+//                   backspaceRemovesValue={false}
+//                   placeholder="Select a state"
+//                   required
+//                   />
              
-            </div>
-            <div class="form-group col-md-2">
-              <label for="inputZip">Zip</label>
-              <input type="text" class="form-control" id="inputZip"/>
-            </div>
-          </div>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="height">Height</label>
-              <input type="height" class="form-control" id="height" placeholder="6 foot 7"/>
-            </div>
-            <div class="form-group col-md-6">
-              <label for="weight">Weight</label>
-              <input type="weight" class="form-control" id="weight" placeholder="186.9"/>
-            </div>
-          </div>
-          <div class="form-group">
-              <label for="allergies">Allergies</label>
-              <textarea class="form-control" id="allergies" rows="3"></textarea>
-            </div>
-          <button type="submit" class="btn btn-primary">Save</button>
-        </form>
-      )
-    }
-   }
+//             </div>
+//             <div class="form-group col-md-2">
+//               <label for="zipcode">Zip</label>
+//               <input type="text" class="form-control" id="zipcode"/>
+//             </div>
+//           </div>
+//           <div class="form-row">
+//             <div class="form-group col-md-6">
+//               <label for="height">Height</label>
+//               <input type="height" class="form-control" id="height" placeholder="6 foot 7"/>
+//             </div>
+//             <div class="form-group col-md-6">
+//               <label for="weight">Weight</label>
+//               <input type="weight" class="form-control" id="weight" placeholder="186.9"/>
+//             </div>
+//           </div>
+//           <div class="form-group">
+//               <label for="allergies">Allergies</label>
+//               <textarea class="form-control" id="allergies" rows="3"></textarea>
+//             </div>
+//           <button type="submit" class="btn btn-primary">Save</button>
+//         </Form>
+//       )
+//     }
+//    }
     
 export default Customer
    
